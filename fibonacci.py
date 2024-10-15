f7dae1e80332a06a50bb1e840e37d4f0be8ffa44
Jaime Cabrera <jaimeibz97@gmail.com>
2024-10-15 10:36:19 +0200

    Remove unnecessary import sys

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,3 @@
-import sys
-
 def find_last_fibonacci(number: int):
     if number < 0:
         raise ValueError("The number must be positive.")
@@ -14,11 +12,9 @@ def find_last_fibonacci(number: int):
     return a
 
 if __name__ == "__main__":
-    import sys
     try:
         number = int(input("Enter a number to receive the largest Fibonacci number less than or equal to it: "))
         last_fib = find_last_fibonacci(number)
         print(last_fib)
     except ValueError as e:
         print(f"Error: {e}")
-        sys.exit(1)
